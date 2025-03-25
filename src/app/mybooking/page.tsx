60e0f16c09efc8586b310a4a5a5807dde9715c52
Hattakorn Hattakarn <hattakorn@MacBook-Pro-khxng-Hattakorn.local>
2025-03-25 12:06:07 +0700

    getBookings

--- src/app/mybooking/page.tsx
+++ src/app/mybooking/page.tsx
@@ -1,5 +1,21 @@
 import BookingList from "@/components/BookingList";
+import getBookings from "@/libs/getBookings";
+import VenueCatalog from "@/components/VenueCatalog";
+import { Suspense } from "react";
+import { LinearProgress } from "@mui/material";
 
+export default function MyBooking () {
+    const booking = getBookings()
+    return (
+        <main className="text-center p-5">
+            <h1 className="text-xl font-medium">Booking List</h1>
+            <Suspense fallback={<p>Loading...<LinearProgress/></p>}>
+                <VenueCatalog venuesJson={booking}/>
+            </Suspense>
+        </main>
+    );
+}
+/*
 export default function MyBooking() {
     return (
         <div className="my-booking">
@@ -8,3 +24,4 @@ export default function MyBooking() {
         </div>
     );
 }
+*/
